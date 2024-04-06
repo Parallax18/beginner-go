14067d9f8f1391926ad0489e392f1cfe8c896186
Parallax18 <rhythmist204@gmail.com>
2024-04-06 03:24:00 +0100

    feat: create slice, update

--- create_slice.go
+++ create_slice.go
@@ -1,7 +1,21 @@
 package main
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
-func createSlice(val string) []string {
-	return strings.Split(val, ",")
+// Write a program which accepts a sequence of comma-separated numbers from console and generate an slice out of them. Return the slice.
+func createSlice(val string) []int {
+
+	//splits string into slice of strings
+	numbers := strings.Split(val, ",")
+
+	length := len(numbers)
+	var num = make([]int, length)       //make an empty slice of the length of the array
+	for index, value := range numbers { //loop through numbers with range
+		num[index], _ = strconv.Atoi(strings.Trim(value, " ")) //update num with integer values after removing whitespace
+	}
+
+	return num
 }
