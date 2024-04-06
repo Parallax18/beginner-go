c22ac1bb5baf9b70af42829ceb6daaf374f87a08
Parallax18 <rhythmist204@gmail.com>
2024-04-06 01:59:50 +0100

    feat: factorial

--- divisible_by_7_not_5.go
+++ divisible_by_7_not_5.go
@@ -5,8 +5,9 @@ import (
 	"strings"
 )
 
+//	Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
+
 func divisibleBy7Not5(lower, upper int) string {
-	//	Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
 
 	var values []string
 	for i := lower; i <= upper; i++ {
--- factorial.go
+++ factorial.go
@@ -2,6 +2,7 @@ package main
 
 import "strconv"
 
+// Write a program which can compute the factorial of a given numbers. The results should be printed in a comma-separated sequence on a single line.
 func factorial(number int) string {
 	var f int = 1 // default value of int is Zero, initialize as 1 to avoid zero multiplication.
 	for i := number; i > 1; i-- {
