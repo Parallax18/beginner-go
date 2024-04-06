10e072e6ff842208a3286d470248573bb6bf0053
Parallax18 <rhythmist204@gmail.com>
2024-04-06 02:15:39 +0100

    feat: map squared numbers

--- map_number_squared.go
+++ map_number_squared.go
@@ -2,6 +2,8 @@ package main
 
 import "math"
 
+//With a given integral number n, write a program to generate a map that contains (i, i*i) such that is an integral number between 1 and n (both included), and then the program should print the map with representation of the value
+
 func mapNumbersSquared(number int) map[int]int {
 	m := make(map[int]int)
 
