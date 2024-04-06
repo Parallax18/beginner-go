9cf3961b31f16fe9725ef121e3fef047f583cb1c
Parallax18 <rhythmist204@gmail.com>
2024-04-06 01:57:09 +0100

    feat: factorial

--- factorial.go
+++ factorial.go
@@ -3,8 +3,8 @@ package main
 import "strconv"
 
 func factorial(number int) string {
-	var f int
-	for i := number; i >= 0; i-- {
+	var f int = 1 // default value of int is Zero, initialize as 1 to avoid zero multiplication.
+	for i := number; i > 1; i-- {
 		f *= i
 	}
 
