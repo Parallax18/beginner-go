ab7edb1ded8f1817a6fe2d0323febfa8faa64c07
Parallax18 <rhythmist204@gmail.com>
2024-04-06 01:33:50 +0100

    first commit: divsible by 7 not 5

---
+++ divisible_by_7_not_5.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func divisibleBy7Not5(lower, upper int) string {
+	//	Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+	var values []string
+	for i := lower; i <= upper; i++ {
+		if i%7 == 0 && i%5 != 0 {
+			values = append(values, strconv.Itoa(i))
+
+		}
+	}
+	return strings.Join(values, ",")
+
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var print = fmt.Println
+
+func main() {
+	print(divisibleBy7Not5(2000, 3200))
+}
